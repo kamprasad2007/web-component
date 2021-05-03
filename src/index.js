a0b2f9c54da8e380536a11a3a707e7c2f52c993c
Manoj Prasad <kamprasad2007@gmail.com>
2021-05-03 21:09:01 +0800

    new change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './web-components/custom-element';
 
+const App = () => {
+
+  useEffect(()=>{
+    document.addEventListener('mouseClicked', e => console.log(e.detail))
+  },[]);
+
+  return(
+    <>
+      <div>Web components</div>
+      <custom-element label="Testing" />
+    </>
+  )
+}
+
 ReactDOM.render(
-  <>
-    <div>Web components</div>
-    <custom-element  label="Testing"/>
-  </>,
+  <App />
+,
   document.getElementById('root')
 );
--- src/web-components/custom-element.js
+++ src/web-components/custom-element.js
@@ -1,18 +1,59 @@
+const template = document.createElement("template");
+template.innerHTML = ` 
+<style>
+label {
+  color:red;
+}
+</style>
+<label>This is from Custom Element</label>
+`;
+
 class CustomElement extends HTMLElement {
-    // constructor() {
-    //   super();
-    //   this.shadow = this.attachShadow({ mode: "open" });
-    // }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
 
-    static get observedAttributes() {
-      return ["label"];
-    }
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.$label = this.shadowRoot.querySelector("label");
+    this.$label.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("mouseClicked", {
+          detail: "hello from custom element to parent",
+          bubbles: true
+        })
+      );
+    });
+  }
+
+  // getter and setter to access attributes
+  get label() {
+    return this.getAttribute("label");
+  }
+
+  set label(value) {
+    this.setAttribute("label", value);
+  }
+
+
   
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.log(name, oldValue, newValue)
-    }
+  // lifecycle callbacks
+  static get observedAttributes() {
+    return ["label"];
+  }
 
-    connectedCallback() {
+  // lifecycle callbacks
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name);
+  }
+
+  // lifecycle callbacks
+  disconnectedCallback() {}
+
+  // lifecycle callbacks
+  adoptedCallback() {}
+
+  // lifecycle callbacks
+  connectedCallback() {
     //   this.shadow.innerHTML = `
     //   <style>
     //     label {
@@ -20,11 +61,7 @@ class CustomElement extends HTMLElement {
     //     }
     //   </style>
     //  <label>This is from Custom Element ${this.getAttribute('label')}</label>`;
-
-     this.innerHTML = `
-     <label>This is from Custom Element ${this.getAttribute('label')}</label>`;
-    }
   }
-  
-  window.customElements.define("custom-element", CustomElement);
-  
+}
+
+window.customElements.define("custom-element", CustomElement);
